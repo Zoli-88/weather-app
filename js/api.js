dd9f508f8c84a3d07e0fd7b6182f314b1cea09dd
Zoltán H <zoltan.h@wavemyth.com>
2023-02-13 17:05:03 +0200

    started implementing geolocation functionality

--- js/api.js
+++ js/api.js
@@ -1,7 +1,32 @@
 
-async function getWeatherInfo() {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Cluj-Napoca&appid=ad6751393ee2ba66fdf0d09e46a0cc51");
+async function getWeatherInfo(lat, lon) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad6751393ee2ba66fdf0d09e46a0cc51`);
     const data = await response.json();
     console.log(data);
+    // 46.77372306958517, 23.62372585794322
 }
-getWeatherInfo();
+
+let currentLocation = {
+    lat: null,
+    lon: null
+};
+
+const GEOLOCATION_OPTIONS = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function getGeolocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+        currentLocation.lat = position.coords.latitude;
+        currentLocation.lon = position.coords.longitude;
+    });
+    getWeatherInfo(currentLocation.lat, currentLocation.lon);
+}
+
+function handleErrorGeolocation(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(getGeolocation, handleErrorGeolocation, GEOLOCATION_OPTIONS);
